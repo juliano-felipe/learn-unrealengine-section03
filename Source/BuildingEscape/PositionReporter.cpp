6531580e432273c6207ef5a6452c151c998e03a9
Juliano Felipe <jotafelipe@gmail.com>
2017-08-26 02:44:33 -0300

    create all walls
    Signed-off-by: Juliano Felipe <jotafelipe@gmail.com>

--- Source/BuildingEscape/PositionReporter.cpp
+++ Source/BuildingEscape/PositionReporter.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReporter.h"
+#include "GameFramework/Actor.h"
 
 
 // Sets default values for this component's properties
@@ -9,8 +10,7 @@ UPositionReporter::UPositionReporter()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
+    
 }
 
 
@@ -18,9 +18,9 @@ UPositionReporter::UPositionReporter()
 void UPositionReporter::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
-	
+    FString sObjectName = GetOwner()->GetName();
+    FString sObjectPos = GetOwner()->GetTransform().GetLocation().ToString();
+    UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *sObjectName, *sObjectPos);
 }
 
 
